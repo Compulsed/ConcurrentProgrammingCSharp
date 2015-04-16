49a27ac8e1f44acb968dde9a37960e256fb6b2b7
Dale Salter <djsalter93@hotmail.com>
2015-04-16 19:15:09 +1000

    Added ParallelProgramming

---
+++ DSalter.Submissions/17-ParallelProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DSalter.ConcurrentUtils;
+using Thread = System.Threading.Thread;
+
+namespace DSalter.Submissions
+{
+	public class _7_ParallelProgram
+	{
+		public _7_ParallelProgram ()
+		{
+		}
+	}
+}
+
